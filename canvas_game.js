3d5b9089d74d8c2eefb015b54be81863b83ea4c4
codeclub <codeclub@LAUSD-MBP-2017-4.local>
2017-11-30 19:16:49 -0800

    Added Lives counter and decrement with left wall

--- canvas_game.js
+++ canvas_game.js
@@ -2,11 +2,14 @@ window.onload=game;
 
 function game(){
 console.log("New Game Loaded");
+
+//dom assignment
 var cvs=document.getElementById("cvs");
 var ctx=cvs.getContext('2d');
+var lives_display = document.getElementById("lives");
 var circle_start=0*Math.PI;var circle_end=2*Math.PI;
 var radius = 25;
-
+var lives = 10;
 var speed_horizontal = 5;
 var speed_vertical = 5;
 var x_pos = 25;
@@ -15,8 +18,22 @@ var y_pos = 25;
 // ctx.fillRect(x_pos,y_pos,box_width,box_height);
 // console.log(x_pos,y_pos,box_width,box_height);
 
+//paddles 
+
+
+//lives
+
+
 function move(){
 
+//miss check
+if(x_pos<25){
+    if(lives>=0){lives--}
+    else (gameover());
+
+    
+}
+lives_display.innerHTML = lives;
 // wall collision check     
 if(x_pos < 25 || x_pos >675){speed_horizontal = -1*(speed_horizontal)} ;
 if(y_pos < 25 || y_pos >475){speed_vertical = -1*(speed_vertical)}; 

7ca702acd364a87594e4640872439e85f9d1f869
codeclub <codeclub@LAUSD-MBP-2017-4.local>
2017-11-30 17:47:02 -0800

    Got ball bouncing increased stage size

--- canvas_game.js
+++ canvas_game.js
@@ -4,39 +4,39 @@ function game(){
 console.log("New Game Loaded");
 var cvs=document.getElementById("cvs");
 var ctx=cvs.getContext('2d');
-var box_width=50;
-var box_height=50;
+var circle_start=0*Math.PI;var circle_end=2*Math.PI;
+var radius = 25;
 
 var speed_horizontal = 5;
 var speed_vertical = 5;
-var x_pos = 1;
-var y_pos = 1;
-ctx.fillStyle='red';
-ctx.fillRect(x_pos,y_pos,box_width,box_height);
-console.log(x_pos,y_pos,box_width,box_height);
+var x_pos = 25;
+var y_pos = 25;
+
+// ctx.fillRect(x_pos,y_pos,box_width,box_height);
+// console.log(x_pos,y_pos,box_width,box_height);
 
 function move(){
-if(x_pos < 0 || x_pos >550){speed_horizontal = -1*(speed_horizontal)} ;
-if(y_pos < 0 || y_pos >350){speed_vertical = -1*(speed_vertical)};   
-ctx.clearRect(0,0,600,400);        
+
+// wall collision check     
+if(x_pos < 25 || x_pos >675){speed_horizontal = -1*(speed_horizontal)} ;
+if(y_pos < 25 || y_pos >475){speed_vertical = -1*(speed_vertical)}; 
+
+// draw the circle
+ctx.clearRect(0,0,700,500);        
 ctx.fillStyle='red';
-ctx.fillRect(x_pos,y_pos,box_width,box_height);
+ctx.beginPath();
+ctx.arc(x_pos,y_pos,radius,circle_start,circle_end);
+ctx.fill();
+
+// keep it moving!! 
 x_pos+=speed_horizontal;y_pos+=speed_vertical;
-// console.log(speed_vertical,speed_horizontal);
+
 
 requestAnimationFrame(move);
     }
 
-   
+    console.log(circle_start,circle_end)
 
 
-    console.log("end of code")
-
-function newfunc(){
-    move();
-    console.log("this func works")
-}
-// move();
-
-newfunc();
+ move();
 }

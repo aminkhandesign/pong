e2e4748262dfa841f9f50104a9c8f86a96804430
Administrator <administrator@LAUSD-MBP-2017-4.local>
2017-12-28 12:54:33 -0800

    setting paddle movement limits

--- canvas_game.js
+++ canvas_game.js
@@ -22,7 +22,7 @@ var move_amount = 1;
 // ctx.fillRect(x_pos,y_pos,box_width,box_height);
 // console.log(x_pos,y_pos,box_width,box_height);
 
-//paddles 
+//paddles
 var keys = [];
 document.addEventListener("keydown",function(ev){
     keys[ev.keyCode]=true;
@@ -39,7 +39,7 @@ document.addEventListener("keyup",function(ev){
 
 function game(){
 
-    ctx.clearRect(0,0,700,500);  
+    ctx.clearRect(0,0,700,500);
 
 
 
@@ -48,10 +48,10 @@ function game(){
 //     if(lives>=0){lives--}
 //     else (gameover());
 
-    
+
 // }
 lives_display.innerHTML = lives;
-// wall collision check    
+// wall collision check
 if((x_pos < 50 && (y_pos<paddle_bottom && y_pos>paddle_top)) || x_pos >675){speed_horizontal = -speed_horizontal} ;
 //the above condition checks for i) touching the LEFT side (x_pos<50) AND  touching the left paddle OR  its touching right hand size(x_pos>675)
 //reverses horizontal speed
@@ -60,16 +60,16 @@ if((x_pos < 50 && (y_pos<paddle_bottom && y_pos>paddle_top)) || x_pos >675){spee
 if(x_pos < 25 && (y_pos>paddle_bottom || y_pos<paddle_top)) {
     speed_horizontal = -speed_horizontal;
     if(lives>0){lives--}
-    else if (lives==0){gameover()};    
+    else if (lives==0){gameover()};
 }
-//this checks if the ball is  touching left wall and  NOT touching the paddle , 
+//this checks if the ball is  touching left wall and  NOT touching the paddle ,
 //reverses horizontal speed, also checks and adjusts lives, invokes gameover() is less than 0
 
-if(y_pos < 25 || y_pos >475){speed_vertical = -speed_vertical}; 
+if(y_pos < 25 || y_pos >475){speed_vertical = -speed_vertical};
 //changes vertical speed if touching top or bottom walls
 
 function gameover(){
-    document.body.innerHTML="game over";  
+    document.body.innerHTML="game over";
 }
 //game over function simply replacing contents with 'game over'
 
@@ -77,32 +77,47 @@ function gameover(){
 //rendering elements
 
 // draw the circle
-      
+
 ctx.fillStyle='red';
 ctx.beginPath();
 ctx.arc(x_pos,y_pos,radius,circle_start,circle_end);
 ctx.fill();
 
-// keep it moving!! 
+// keep it moving!!
 x_pos+=speed_horizontal;y_pos+=speed_vertical;
 
 
 //draw retangle
 
-if(keys[38])
+if(keys[38] && paddle_top>15)
 {
     //move_amount *= friction;
+    //check if hitting walls
     move_amount--;
     if(move_amount<-5){move_amount=-5}
     console.log("key press detected")
 }
-if(keys[40]){
+else if (keys[38] && paddle_top<15) {
+  move_amount=20;
+  console.log("woops")
+}
+
+if((keys[40]) && paddle_top<400){
     //move_amount *= -friction;
     move_amount++;
-    if(move_amount>5){move_amount=5}
+    if(move_amount>=15){move_amount=5}
     console.log("key UP detected")
 }
-paddle_top+=move_amount;
+else if ((keys[40]) && paddle_top>400)
+{  move_amount= -20;
+  console.log("woops")}
+
+
+
+if(paddle_top>15 || paddle_top<400){
+paddle_top+=move_amount;}
+else{paddle_top-=move_amount}
+
 ctx.fillStyle = 'orangered';
 ctx.fillRect(0,paddle_top,30,150);
 
@@ -116,7 +131,7 @@ ctx.fillRect(0,paddle_top,30,150);
 requestAnimationFrame(game);
     }
 
- 
+
 
 
   game();
